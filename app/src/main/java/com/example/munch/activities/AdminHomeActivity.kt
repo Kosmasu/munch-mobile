034d7071d1752c9f171d8945ad5ec8406618d48d
Kenny <ken_link26@yahoo.com>
2022-12-01 13:07:12 +0700

    init admin fragment and add assets

--- app/src/main/java/com/example/munch/activities/AdminHomeActivity.kt
+++ app/src/main/java/com/example/munch/activities/AdminHomeActivity.kt
@@ -3,11 +3,26 @@ package com.example.munch.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.munch.R
+import com.example.munch.databinding.ActivityAdminHomeBinding
 
 class AdminHomeActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityAdminHomeBinding
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_admin_home)
+    binding = ActivityAdminHomeBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.menu_option_logout, menu)
+    return super.onCreateOptionsMenu(menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    finish()
+    return super.onOptionsItemSelected(item)
   }
 }
---
+++ app/src/main/java/com/example/munch/fragments/AdminCustomerFragment.kt
@@ -0,0 +1,32 @@
+package com.example.munch.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.munch.R
+import com.example.munch.databinding.FragmentAdminCustomerBinding
+import com.example.munch.databinding.FragmentAdminHomeBinding
+
+class AdminCustomerFragment : Fragment() {
+  var _binding: FragmentAdminCustomerBinding? = null
+  val binding get() = _binding!!
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+  }
+
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?,
+  ): View? {
+    _binding = FragmentAdminCustomerBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
+}
