c7f58a765e86df5939932bd2263f8154480ae0b5
geazo <jasonsteam9672@gmail.com>
2022-12-28 20:55:08 +0700

    Admin home Layout

--- app/src/main/java/com/example/munch/activities/AdminHomeActivity.kt
+++ app/src/main/java/com/example/munch/activities/AdminHomeActivity.kt
@@ -7,6 +7,7 @@ import android.view.Menu
 import android.view.MenuItem
 import com.example.munch.R
 import com.example.munch.databinding.ActivityAdminHomeBinding
+import com.example.munch.fragments.*
 
 class AdminHomeActivity : AppCompatActivity() {
   private lateinit var binding: ActivityAdminHomeBinding
@@ -14,6 +15,48 @@ class AdminHomeActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     binding = ActivityAdminHomeBinding.inflate(layoutInflater)
     setContentView(binding.root)
+
+
+    supportFragmentManager.beginTransaction().apply {
+      replace(binding.flFragmentAdmin.id,AdminHomeFragment() , "AdminHomeFragment")
+      setReorderingAllowed(true)
+      commit()
+    }
+
+    binding.bnvAdmin.setOnItemSelectedListener {
+      when (it.title.toString().lowercase()) {
+        "home" -> {
+          supportFragmentManager.beginTransaction().apply {
+            replace(binding.flFragmentAdmin.id, AdminHomeFragment(), "GuestLoginFragment")
+            setReorderingAllowed(true)
+            commit()
+          }
+        }
+        "customer" -> {
+          supportFragmentManager.beginTransaction().apply {
+            replace(binding.flFragmentAdmin.id,AdminCustomerFragment() , "AdminHomeFragment")
+            setReorderingAllowed(true)
+            commit()
+          }
+        }
+        "provider" -> {
+          supportFragmentManager.beginTransaction().apply {
+            replace(binding.flFragmentAdmin.id,AdminProviderFragment() , "AdminHomeFragment")
+            setReorderingAllowed(true)
+            commit()
+          }
+        }
+        "history" -> {
+          supportFragmentManager.beginTransaction().apply {
+            replace(binding.flFragmentAdmin.id,AdminHistoryFragment() , "AdminHomeFragment")
+            setReorderingAllowed(true)
+            commit()
+          }
+        }
+      }
+      return@setOnItemSelectedListener true
+    }
+
   }
 
   override fun onCreateOptionsMenu(menu: Menu?): Boolean {
