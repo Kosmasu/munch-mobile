7aa421867a5f9be54f70343e0fff8d3e2c897fd2
Ignatius Odi <ignodi@gmail.com>
2023-01-03 15:34:25 +0700

    admin_home (-approveProvider)

--- app/src/main/java/com/example/munch/fragments/AdminHomeFragment.kt
+++ app/src/main/java/com/example/munch/fragments/AdminHomeFragment.kt
@@ -1,16 +1,37 @@
 package com.example.munch.fragments
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.munch.R
+import com.example.munch.adapter.AdminUserAdapter
+import com.example.munch.api.Retrofit
+import com.example.munch.api.auth.AuthStore
+import com.example.munch.api.auth.MyStatResponse
+import com.example.munch.api.user.UserStore
 import com.example.munch.databinding.FragmentAdminHomeBinding
+import com.example.munch.model.Response
+import com.example.munch.model.User
+import kotlinx.coroutines.launch
 
 class AdminHomeFragment : Fragment() {
   var _binding: FragmentAdminHomeBinding? = null
   val binding get() = _binding!!
+
+  var reqMap : Map<String, String> = mapOf("users_role" to "provider", "users_status" to "menunggu")
+  var listWaitingProvider : List<User> = listOf()
+  var stats : MyStatResponse? = null
+  lateinit var authStore : AuthStore
+  lateinit var userStore : UserStore
+  lateinit var providerAdapter : AdminUserAdapter
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -22,6 +43,63 @@ class AdminHomeFragment : Fragment() {
   ): View? {
     _binding = FragmentAdminHomeBinding.inflate(inflater, container, false)
     return binding.root
+  }
+
+  @SuppressLint("SetTextI18n")
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    authStore = AuthStore.getInstance(requireContext())
+    userStore = UserStore.getInstance(requireContext())
+    println("HELLO")
+    Retrofit.coroutine.launch {
+      stats = authStore.myStat().data
+      listWaitingProvider = userStore.fetchUnpaginated(reqMap).data
+
+      (requireContext() as Activity).runOnUiThread {
+        binding.tvRegisteredAccounts.text = "${stats?.providers_count?.let {
+          stats?.customers_count?.plus(
+            it
+          )
+        }} Accounts"
+        binding.tvCustomers.text = "${stats?.customers_count} Accounts"
+        binding.tvProviders.text = "${stats?.providers_count} Accounts"
+
+        providerAdapter = AdminUserAdapter(listWaitingProvider)
+        binding.rvListWaitingProvider.addItemDecoration(DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL))
+        binding.rvListWaitingProvider.adapter = providerAdapter
+        binding.rvListWaitingProvider.layoutManager = LinearLayoutManager(requireContext())
+        providerAdapter.notifyDataSetChanged()
+
+        providerAdapter.onClickListener = fun (it: View, position: Int, user: User) {
+          val popUp = PopupMenu(requireContext(), it)
+          popUp.menuInflater.inflate(R.menu.menu_popup_approve, popUp.menu)
+          popUp.setOnMenuItemClickListener {
+            return@setOnMenuItemClickListener when(it.itemId) {
+              R.id.menu_popup_approve -> {
+                approve(user.users_id, requireContext())
+                true
+              }
+              else -> {
+                false
+              }
+            }
+          }
+          popUp.show()
+        }
+      }
+    }
+  }
+
+  fun approve(id: ULong, context: Context) {
+    Retrofit.coroutine.launch {
+//      userStore.approve(id)
+      listWaitingProvider = userStore.fetchUnpaginated(reqMap).data
+      (context as Activity).runOnUiThread {
+        providerAdapter.data = listWaitingProvider
+        providerAdapter.notifyDataSetChanged()
+      }
+    }
   }
 
   override fun onDestroyView() {
