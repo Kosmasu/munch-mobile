e3212090ae41482dd8444918c602dd08bc962b87
geazo <jasonsteam9672@gmail.com>
2023-01-05 10:56:28 +0100

    topup fragment

---
+++ app/src/main/java/com/example/munch/fragments/CustomerMenuProviderFragment.kt
@@ -0,0 +1,32 @@
+package com.example.munch.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.munch.R
+import com.example.munch.databinding.FragmentCustomerMenuProviderBinding
+import com.example.munch.databinding.FragmentCustomerProfileBinding
+
+class CustomerMenuProviderFragment : Fragment() {
+    var _binding: FragmentCustomerMenuProviderBinding? = null
+    val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCustomerMenuProviderBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+ }
