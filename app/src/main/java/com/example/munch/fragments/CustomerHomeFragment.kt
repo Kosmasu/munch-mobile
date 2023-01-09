b8b5a0fa29c6cd0ff7e8be58b8f6da6178878d1e
Ignatius Odi <ignodi@gmail.com>
2023-01-10 00:34:18 +0700

    customer home setup

--- app/src/main/java/com/example/munch/fragments/CustomerHomeFragment.kt
+++ app/src/main/java/com/example/munch/fragments/CustomerHomeFragment.kt
@@ -1,15 +1,37 @@
 package com.example.munch.fragments
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.munch.api.Retrofit
+import com.example.munch.api.history.HistoryStore
+import com.example.munch.api.pesanan.PesananStore
+import com.example.munch.api.user.UserStore
 import com.example.munch.databinding.FragmentCustomerHomeBinding
+import com.example.munch.model.DetailPemesanan
+import com.example.munch.model.HistoryPemesanan
+import com.example.munch.model.User
+import kotlinx.coroutines.launch
 
 class CustomerHomeFragment : Fragment() {
-    var _binding: FragmentCustomerHomeBinding? = null
+    private var _binding: FragmentCustomerHomeBinding? = null
     val binding get() = _binding!!
+
+    var reqMapCateringAnda : Map<String, String> = mapOf("month" to "1", "year" to "2023", "detail_status" to "terkirim")
+    var reqMapTopCatering : Map<String, String> = mapOf("batch_size" to "5", "sort.column" to "users_rating", "sort.type" to "desc")
+    var reqMapOrderAgain : Map<String, String> = mapOf("batch_size" to "5", "pemesanan_status" to "selesai")
+    lateinit var pesananStore : PesananStore
+    lateinit var userStore : UserStore
+//    lateinit var detailAdapter : CustomerDetailPemesananAdapter
+//    lateinit var userAdapter : CustomerUserAdapter
+//    lateinit var historyAdapter : CustomerHistoryPemesananAdapter
+    var cateringAnda : List<DetailPemesanan> = listOf()
+    var topCatering : List<User> = listOf()
+    var orderAgain : List<HistoryPemesanan> = listOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +40,31 @@ class CustomerHomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
         _binding = FragmentCustomerHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Retrofit.coroutine.launch {
+            try {
+                cateringAnda = pesananStore.fetchDelivery(reqMapCateringAnda).data
+                topCatering = userStore.fetchUnpaginated(reqMapTopCatering).data
+                orderAgain = pesananStore.fetchUnpaginated(reqMapOrderAgain).data
+
+                (context as Activity).runOnUiThread {
+                    println("CATERING ANDA : $cateringAnda")
+                    println("TOP CATERING : $topCatering")
+                    println("ORDER AGAIN : $orderAgain")
+                }
+            } catch (e: Exception) {
+                (context as Activity).runOnUiThread {
+                    Toast.makeText(requireContext(), "Error", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
     }
 
     override fun onDestroyView() {
