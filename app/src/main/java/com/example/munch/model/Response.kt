be64466ba952eda18befd7f5f2a1215d0dc62c30
Kenny <ken_link26@yahoo.com>
2023-01-11 12:01:18 +0700

    add old response

--- app/src/main/java/com/example/munch/model/Response.kt
+++ app/src/main/java/com/example/munch/model/Response.kt
@@ -6,3 +6,12 @@ data class Response<T>(
   val data: T,
 ) {
 }
+
+//package com.example.munch.model
+//
+//data class Response<T>(
+//  val status: String,
+//  val message: String,
+//  val data: T,
+//) {
+//}
