e4545f2ae614c07223a3c2f3d5662916b6e06f85
Kenny <ken_link26@yahoo.com>
2023-01-11 09:23:20 +0700

    ignatius dodi

--- app/src/main/java/com/example/munch/model/Menu.kt
+++ app/src/main/java/com/example/munch/model/Menu.kt
@@ -9,6 +9,7 @@ data class Menu(
   val menu_harga: Long?,
   val menu_status: String?, //enum("tersedia", "tidak tersedia")
   val users_id: ULong,
+  val users: User?,
   val created_at: Timestamp?,
   val updated_at: Timestamp?,
   val deleted_at: Timestamp?,
