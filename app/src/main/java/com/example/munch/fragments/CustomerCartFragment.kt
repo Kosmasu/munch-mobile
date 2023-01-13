c872d6627d1bfc432757313e2f30cd6e241df3aa
CBEngineer9 <CBEalexanderkevin@gmail.com>
2023-01-13 22:31:35 +0700

    fix pesan cart in CustomerCartFragment.kt

--- app/src/main/java/com/example/munch/fragments/CustomerCartFragment.kt
+++ app/src/main/java/com/example/munch/fragments/CustomerCartFragment.kt
@@ -14,22 +14,26 @@ import com.example.munch.activities.CustomerHomeActivity
 import com.example.munch.adapter.CustomerCartAdapter
 import com.example.munch.api.Retrofit
 import com.example.munch.api.cart.CartStore
+import com.example.munch.api.pesanan.PesananStore
 import com.example.munch.databinding.FragmentCustomerCartBinding
 import com.example.munch.helpers.FragmentUtils.isSafeFragment
 import com.example.munch.model.HeaderCart
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
 class CustomerCartFragment : Fragment() {
     private var _binding: FragmentCustomerCartBinding? = null
     val binding get() = _binding!!
 
-    lateinit var cartStore : CartStore
+    private lateinit var cartStore : CartStore
+    private lateinit var pesananStore: PesananStore
     lateinit var cartAdapter : CustomerCartAdapter
-    var listCart : List<HeaderCart> = listOf()
+    var listCart : ArrayList<HeaderCart> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         cartStore = CartStore.getInstance(requireContext())
+        pesananStore = PesananStore.getInstance(requireContext())
     }
 
     override fun onCreateView(
@@ -44,7 +48,9 @@ class CustomerCartFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         Retrofit.coroutine.launch {
             try {
-                listCart = cartStore.fetchUnpaginated().body()?.data!!
+                val cartRes = cartStore.fetchUnpaginated().body()?.data!!
+                listCart.clear()
+                listCart.addAll(cartRes)
 
                 if (isSafeFragment()) {
                     (context as Activity).runOnUiThread {
@@ -69,16 +75,43 @@ class CustomerCartFragment : Fragment() {
             }
         }
 
+        binding.btnOrderCart.setOnClickListener {
+            Retrofit.coroutine.launch {
+                try {
+                    val resp = pesananStore.pesanCart()
+                    if (resp.code() == 402) {
+                        throw HttpException(resp)
+                    }
+
+                    if (isSafeFragment()) {
+                        requireActivity().runOnUiThread {
+                            val deletedCount = listCart.size
+                            listCart.clear()
+                            cartAdapter.notifyItemRangeRemoved(0,deletedCount)
+//                            cartAdapter.notifyDataSetChanged()
+                            Toast.makeText(requireContext(), "Successfully ordered cart", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } catch (e: Exception) {
+                    Log.e("CART ERROR:", "onViewCreated: Error order cart", e)
+                    if (isSafeFragment()) {
+                        (context as Activity).runOnUiThread {
+                            Toast.makeText(requireContext(), "Error order cart", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+        }
+
         binding.btnClearCart.setOnClickListener {
             Retrofit.coroutine.launch {
                 try {
                     cartStore.clear()
-                    listCart = cartStore.fetchUnpaginated().body()?.data!!
 
                     if (isSafeFragment()) {
                         (context as Activity).runOnUiThread {
-                            cartAdapter.data = listCart
-                            cartAdapter.notifyDataSetChanged()
+                            listCart.clear()
+                            cartAdapter.notifyItemRangeRemoved(0,cartAdapter.itemCount)
                             Toast.makeText(requireContext(), "Cart cleared", Toast.LENGTH_SHORT).show()
                         }
                     }
